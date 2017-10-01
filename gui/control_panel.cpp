2bb6c1f6b2e501033d403e3c0647e92679975073
Alexandru Csete <oz9aec@gmail.com>
2017-10-01 15:01:09 +0200

    Implement mode selection
    Add mode selector buttons for AM, SSB, CW and FM. Repeated clicks on the
    SSB button toggles between LSB and USB.

--- gui/control_panel.cpp
+++ gui/control_panel.cpp
@@ -27,40 +27,118 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include <new>
 #include <QDebug>
 
 #include "control_panel.h"
+#include "nanosdr/common/sdr_data.h"
 #include "ui_control_panel.h"
 
+#define CP_MODE_NONE    0
+#define CP_MODE_AM      1
+#define CP_MODE_SAM     2
+#define CP_MODE_LSB     3
+#define CP_MODE_USB     4
+#define CP_MODE_CW      5
+#define CP_MODE_FM      6      // Voice FM, 5.0 kHz
+#define CP_MODE_FMN     7      // Voice FM, 2.5 kHz
+#define CP_MODE_NUM     8
 
 ControlPanel::ControlPanel(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ControlPanel)
 {
+    current_mode = CP_MODE_NONE;
+    last_ssb_mode = CP_MODE_USB;
+
     ui->setupUi(this);
+    initModeSettings();
 }
 
 ControlPanel::~ControlPanel()
 {
+    delete mode_settings;
     delete ui;
+}
+
+void ControlPanel::initModeSettings(void)
+{
+    mode_settings = new(std::nothrow) mode_setting[CP_MODE_NUM];
+    if (!mode_settings)
+        qFatal("%s: Failed to allocate memory for mode array. Aborting.\n",
+               __func__);
+
+    mode_settings[CP_MODE_NONE].demod = SDR_DEMOD_NONE;
+    mode_settings[CP_MODE_NONE].filter_lo = 0.0;
+    mode_settings[CP_MODE_NONE].filter_hi = 0.0;
+    mode_settings[CP_MODE_NONE].cw_offset = 0.0;
+    mode_settings[CP_MODE_AM].demod = SDR_DEMOD_AM;
+    mode_settings[CP_MODE_AM].filter_lo = -5000.0;
+    mode_settings[CP_MODE_AM].filter_hi = 5000.0;
+    mode_settings[CP_MODE_AM].cw_offset = 0.0;
+    mode_settings[CP_MODE_SAM].demod = SDR_DEMOD_AM;
+    mode_settings[CP_MODE_SAM].filter_lo = -5000.0;
+    mode_settings[CP_MODE_SAM].filter_hi = 5000.0;
+    mode_settings[CP_MODE_SAM].cw_offset = 0.0;
+    mode_settings[CP_MODE_LSB].demod = SDR_DEMOD_SSB;
+    mode_settings[CP_MODE_LSB].filter_lo = -2800.0;
+    mode_settings[CP_MODE_LSB].filter_hi = -100.0;
+    mode_settings[CP_MODE_LSB].cw_offset = 0.0;
+    mode_settings[CP_MODE_USB].demod = SDR_DEMOD_SSB;
+    mode_settings[CP_MODE_USB].filter_lo = 100.0;
+    mode_settings[CP_MODE_USB].filter_hi = 2800.0;
+    mode_settings[CP_MODE_USB].cw_offset = 0.0;
+    mode_settings[CP_MODE_CW].demod = SDR_DEMOD_SSB;
+    mode_settings[CP_MODE_CW].filter_lo = -400.0;
+    mode_settings[CP_MODE_CW].filter_hi = 400.0;
+    mode_settings[CP_MODE_CW].cw_offset = 700.0;
+    mode_settings[CP_MODE_FM].demod = SDR_DEMOD_FM;
+    mode_settings[CP_MODE_FM].filter_lo = -10000.0;
+    mode_settings[CP_MODE_FM].filter_hi = 10000.0;
+    mode_settings[CP_MODE_FM].cw_offset = 0.0;
+    mode_settings[CP_MODE_FMN].demod = SDR_DEMOD_FM;
+    mode_settings[CP_MODE_FMN].filter_lo = -7500.0;
+    mode_settings[CP_MODE_FMN].filter_hi = 7500.0;
+    mode_settings[CP_MODE_FMN].cw_offset = 0.0;
+}
+
+void ControlPanel::updateMode(quint8 mode)
+{
+    Q_ASSERT(mode < CP_MODE_NUM);
+    qDebug("%s: %u -> %u", __func__, current_mode, mode);
+
+    current_mode = mode;
+    emit    demodChanged(mode_settings[mode].demod);
+    emit    filterChanged(mode_settings[mode].filter_lo,
+                          mode_settings[mode].filter_hi);
+    emit    cwOffsetChanged(mode_settings[mode].cw_offset);
 }
 
 void ControlPanel::on_amButton_clicked(bool)
 {
-    qDebug() << __func__;
+    updateMode(CP_MODE_AM);
 }
 
 void ControlPanel::on_ssbButton_clicked(bool)
 {
-    qDebug() << __func__;
+    quint8    new_mode;
+
+    if (current_mode == CP_MODE_LSB)
+        new_mode = last_ssb_mode = CP_MODE_USB;
+    else if (current_mode == CP_MODE_USB)
+        new_mode = last_ssb_mode = CP_MODE_LSB;
+    else
+        new_mode = last_ssb_mode;
+
+    updateMode(new_mode);
 }
 
 void ControlPanel::on_cwButton_clicked(bool)
 {
-    qDebug() << __func__;
+    updateMode(CP_MODE_CW);
 }
 
 void ControlPanel::on_fmButton_clicked(bool)
 {
-    qDebug() << __func__;
+    updateMode(CP_MODE_FM);
 }
--- gui/control_panel.h
+++ gui/control_panel.h
@@ -4,6 +4,8 @@
 #pragma once
 
 #include <QWidget>
+#include "nanosdr/common/datatypes.h"
+#include "nanosdr/common/sdr_data.h"
 
 namespace Ui {
     class ControlPanel;
@@ -18,7 +20,9 @@ public:
     ~ControlPanel();
 
 signals:
-    void    demodChanged(quint8 new_demod);
+    void    demodChanged(sdr_demod_t new_demod);
+    void    filterChanged(real_t low_cut, real_t high_cut);
+    void    cwOffsetChanged(real_t offset);
 
 private slots:
     void    on_amButton_clicked(bool);
@@ -27,5 +31,22 @@ private slots:
     void    on_fmButton_clicked(bool);
 
 private:
+    void    initModeSettings(void); // FIXME: Replace with a readSettings()
+    void    updateMode(quint8);
+
+private:
     Ui::ControlPanel   *ui;
+
+    quint8              current_mode; // CP_MODE_...
+    quint8              last_ssb_mode;
+
+    // Mode definitions and settings
+    struct  mode_setting {
+        sdr_demod_t    demod;
+        real_t         filter_lo;
+        real_t         filter_hi;
+        real_t         cw_offset;
+    };
+
+    struct mode_setting   *mode_settings; // latest settings for each mode
 };
